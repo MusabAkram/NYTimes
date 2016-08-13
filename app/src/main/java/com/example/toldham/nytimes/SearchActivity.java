0f886edfe0e17319b70facdc559727940547a5c9
Terra Oldham <toldham@hearsaycorp.com>
2016-08-13 10:26:58 -0700

    HTTP Response Added

--- app/src/main/java/com/example/toldham/nytimes/SearchActivity.java
+++ app/src/main/java/com/example/toldham/nytimes/SearchActivity.java
@@ -3,13 +3,23 @@ package com.example.toldham.nytimes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -57,7 +67,31 @@ public class SearchActivity extends AppCompatActivity {
     public void onArticleSearch(View view){
         String query = etQuery.getText().toString();
 
+        //Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+        Log.v("stuff", url);
 
-        Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
+        RequestParams params = new RequestParams();
+        params.put("api-key", "8510b204c17d4582abacf3afb9be55e5");
+        params.put("page", "0");
+        params.put("q", query);
+
+        Log.v("stuff", url);
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //super.onSuccess(statusCode, headers, response);
+                //Log.d("DEBUG", response.toString());
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    Log.d("DEBUG", response.toString());
+                } catch (JSONException e){
+                        e.printStackTrace();
+                    }
+            }
+        } );
     }
 }
