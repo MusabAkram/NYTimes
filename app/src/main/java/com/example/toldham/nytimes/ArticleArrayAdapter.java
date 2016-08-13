cf9ff050c5e325f8426be70ba5ddd4af7790487e
Terra Oldham <toldham@hearsaycorp.com>
2016-08-13 11:35:49 -0700

    Images and Titles added to article items

--- app/src/main/java/com/example/toldham/nytimes/ArticleArrayAdapter.java
+++ app/src/main/java/com/example/toldham/nytimes/ArticleArrayAdapter.java
@@ -1,7 +1,15 @@
 package com.example.toldham.nytimes;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -12,5 +20,36 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
 
     public ArticleArrayAdapter (Context context, List<Article> articles) {
         super (context, android.R.layout.simple_list_item_1, articles);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        //get the data item for the position
+        Article article = this.getItem(position);
+
+        //check if existing view is being recycled
+        //not recycling; inflate layout
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_article_result, parent, false);
+        }
+        //find image view
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivImage);
+        //clear out recycled convertView from last time
+        imageView.setImageResource(0);
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        tvTitle.setText(article.getHeadline());
+
+        //populate thumbnail
+        //remote download image
+
+        String thumbnail = article.getThumbnail();
+        if (!TextUtils.isEmpty(thumbnail)) {
+            Picasso.with(getContext()).load(thumbnail).into(imageView);
+        }
+
+        return convertView;
+
     }
 }
