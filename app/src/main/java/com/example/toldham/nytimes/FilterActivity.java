91e692a10d90b161424c03a3f5e8d8536381f652
Terra Oldham <toldham@hearsaycorp.com>
2016-08-15 00:04:24 -0300

    Filter Activities

--- app/src/main/java/com/example/toldham/nytimes/FilterActivity.java
+++ app/src/main/java/com/example/toldham/nytimes/FilterActivity.java
@@ -1,8 +1,15 @@
 package com.example.toldham.nytimes;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -10,8 +17,21 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapterSpinner;
+        List<String> list;
+
+        list = new ArrayList<String>();
+        list.add("Oldest");
+        list.add("Newest");
+        adapterSpinner = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_item, list);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterSpinner);
     }
 
+    public void onUpdateFilter(View view) {
+        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+        startActivity(intent);
+    }
 }
